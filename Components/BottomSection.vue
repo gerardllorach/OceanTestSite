db2b553be5b5c121bb4d2c3451351f2d6777f1fd
Gerard Llorach <gerard.llorach@gmail.com>
2024-07-09 11:54:35 +0200

    animated show/hide bottom section

--- Components/BottomSection.vue
+++ Components/BottomSection.vue
@@ -1,10 +1,13 @@
 <template>
   <!-- Container -->
-  <div id="bottom-section">
-
+  <div id="bottom-section" :class="[isSectionOpen ? 'bottom-section-open' : 'bottom-section-closed']">
+    <!-- Button to open and close section -->
     <div class="section-opener-container">
-      <button class="section-opener-button clickable" @click="wavesPropsClicked"><span>Menu</span><span
-        class="fa">&#xf106;</span></button>
+      <button class="section-opener-button clickable" @click="bottomSectionClicked"><span>Menu</span>
+        <span class="fa"
+          :class="[isSectionOpen ? 'rotate180' : 'rotate0']"  
+        >&#xf106;</span>
+      </button>
     </div>
 
   </div>
@@ -13,6 +16,29 @@
 
 <script>
 
+export default {
+  name: "BottomSection",
+  created() {},
+  mounted() {
+
+  },
+  data () {
+    return {
+      isSectionOpen: false
+    }
+  },
+  methods: {
+    // Shows / Hides bottom section
+    bottomSectionClicked: function (e){
+      this.isSectionOpen = !this.isSectionOpen;
+      setTimeout(() => {
+        window.dispatchEvent(new Event('resize'));
+      }, 750);
+    },
+  }
+}
+
+
 </script>
 
 <style scoped>
@@ -20,7 +46,16 @@
   position: relative;
   width: 100%;
   background: blue;
+}
+
+.bottom-section-open {
   height: 100%;
+  transition: all 0.7s ease-in-out;
+}
+
+.bottom-section-closed {
+  height: 0;
+  transition: all 0.7s ease-in-out;
 }
 
 .section-opener-container {
@@ -29,7 +64,14 @@
   margin: -60px;
 }
 
-.section-opener-button{
 
+
+.rotate0 {
+  rotate: 0deg;
+  transition: all 0.7s ease-in-out;
+}
+.rotate180 {
+  rotate: 180deg;
+  transition: all 0.7s ease-in-out;
 }
 </style>
